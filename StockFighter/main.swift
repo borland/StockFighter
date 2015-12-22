74760082254eb7cdcb40e20e3c329fe1beb0293d
Orion Edwards <orion.edwards@gmail.com>
2015-12-23 06:28:34 +1300

    Initial Commit

---
+++ StockFighter/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  StockFighter
+//
+//  Created by Orion Edwards on 23/12/15.
+//  Copyright © 2015 Orion Edwards. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
