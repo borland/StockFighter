fe5d77912c4876610124887d9032ae437f1a5299
Orion Edwards <orion.edwards@gmail.com>
2016-02-13 06:49:54 +1300

    Tweaked the algorithm to reliably win level 3 every time. I have a reliable market maker!

--- StockFighter/TradingEngine.swift
+++ StockFighter/TradingEngine.swift
@@ -10,7 +10,7 @@ import Foundation
 
 // Future: tracking a stock across multiple venues? Would require a refactor
 class TradingEngine {
-    struct OutstandingOrder {
+    struct Order {
         let id:Int
         let symbol:String
         let price:Int
@@ -30,15 +30,15 @@ class TradingEngine {
     private var _tapeWebsockets:[String:WebSocketClient] = [:]
     private var _quoteHistory:[QuoteResponse] = [] // we keep the last n quotes in memory
     
-    private var _expenses:Int = 0
-    private var _income:Int = 0
-    
     private var _position:[String:Int] = [:] // key:Stock symbol, value:How many we have
     private var _lastQuotes:[String:QuoteResponse] = [:] // key:Stock symbol
 
-    private var _currentOrders:[Int:OutstandingOrder] = [:] // order's we've placed
+    private var _currentOrders:[Int:Order] = [:] // order's we've placed
     private var _lastPendingOrderId = 0
-    private var _pendingOrders:[Int:OutstandingOrder] = [:] // orders we've sent to the server but haven't received the HTTP response for yet
+    private var _pendingOrders:[Int:Order] = [:] // orders we've sent to the server but haven't received the HTTP response for yet
+    private var _cancelingOrders:[Int:Order] = [:] // orders we've asked the server to cancel but haven't received an HTTP response yet
+    
+    private var _inventory:[Order] = [] // things I've bought
     
     init(apiClient:StockFighterApiClient, account:String, venue:String) {
         _apiClient = apiClient
@@ -66,7 +66,7 @@ class TradingEngine {
                     guard let _ = self._currentOrders[order.id] else { return } // activity from someone else; not tracking this yet
                     
                     if !order.open {
-                        print("completed an order")
+//                        print("completed an order")
                         self.processCompletedOrder(order)
                     }
                 }
@@ -106,8 +106,6 @@ class TradingEngine {
     
     var position:[String:Int] { return lock(self) { _position } }
     
-    var netProfit:Int { return lock(self) { _income - _expenses } }
-    
     /** If there aren't count previous quotes for which selector doesn't return nil, return nil */
     func mapReduceLastQuotes(count:Int, map:(QuoteResponse) -> Int?, reduce:([Int] -> Int)) -> Int? {
         let qh = lock(self) { _quoteHistory } // swift arrays are by value
@@ -137,7 +135,7 @@ class TradingEngine {
     /** Gets the outstanding orders you have placed for a given stock.
      - Parameter symbol: The stock symbol
      - Returns: Array of outstanding orders */
-    func outstandingOrdersForStock(symbol:String) -> [OutstandingOrder] {
+    func outstandingOrdersForStock(symbol:String) -> [Order] {
         return lock(self) {
             _currentOrders.values.filter{ $0.symbol == symbol } +
                 _pendingOrders.values.filter{ $0.symbol == symbol }
@@ -159,33 +157,39 @@ class TradingEngine {
     }
     
     @warn_unused_result
-    func buyStock(symbol:String, price: Int, qty: Int, timeout:NSTimeInterval? = nil) -> Observable<OutstandingOrder> {
+    func buyStock(symbol:String, price: Int, qty: Int, timeout:NSTimeInterval? = nil) -> Observable<Order> {
         return placeOrder(.Buy, symbol, price, qty, timeout)
     }
     
     @warn_unused_result
-    func sellStock(symbol:String, price: Int, qty: Int, timeout:NSTimeInterval? = nil) -> Observable<OutstandingOrder> {
+    func sellStock(symbol:String, price: Int, qty: Int, timeout:NSTimeInterval? = nil) -> Observable<Order> {
         return placeOrder(.Sell, symbol, price, qty, timeout)
     }
     
     @warn_unused_result
-    func cancelOrder(oo:OutstandingOrder) -> Observable<Void> {
-        if let targetId = lock(self, block:{ _currentOrders[oo.id]?.id }) {
-            return _venue.cancelOrderForStockAsync(oo.symbol, id: targetId).map { response in
+    func cancelOrder(oo:Order) -> Observable<Void> {
+        return lock(self){
+            if let order = _currentOrders[oo.id] {
+                _currentOrders[oo.id] = nil
+                _cancelingOrders[oo.id] = order
                 
-                lock(self) {
-                    // the order may have been filled!
-                    self.processCompletedOrder(response)
+                return _venue.cancelOrderForStockAsync(oo.symbol, id: oo.id).map { response in
+                    
+                    lock(self) {
+                        // the order may have been filled!
+                        self.processCompletedOrder(response)
+                    }
+                    if response.fills.count > 0 {
+                        print("cancellation rejected, \(oo.direction) was filled at price \(response.price)")
+                    }
+                    else {
+                        print("canceled \(oo.direction) \(oo.id) at price \(oo.price)")
+                    }
                 }
-                if response.fills.count > 0 {
-                    print("cancellation rejected, order was filled at price \(response.price)")
-                }
-                else {
-                    print("canceled order \(targetId) at price \(oo.price)")
-                }
+
             }
+            return Observable.empty()
         }
-        return Observable.empty()
     }
     
     /** Shuts down the trading engine, closing all websockets and cancelling any outstanding unfilled orders.
@@ -218,12 +222,15 @@ class TradingEngine {
             switch(order.direction) {
             case .Buy:
                 // if we bought some stocks, we spent that money and have more of those stocks
-                _expenses += order.fills.reduce(0){ (m,x) in m + x.qty * x.price }
+//                _inventory.append(Order(id:order.id, symbol:order.symbol, price:order.price, qty:order.originalQty, direction:.Buy))
                 
                 _position[order.symbol] = position + order.fills.reduce(0){ (m,x) in m + x.qty }
             case .Sell:
                 // if we sold some stocks, we gained that money and have less of those stocks
-                _income += order.fills.reduce(0){ (m,x) in m + x.qty * x.price }
+
+                // if I bought THESE STOCKS for $2, I want to sell them for $2.50 or whatever the spread is, and no less than that
+                
+                // remove them from the inventory??
                 
                 _position[order.symbol] = position - order.fills.reduce(0){ (m,x) in m + x.qty }
             }
@@ -232,16 +239,16 @@ class TradingEngine {
     }
     
     @warn_unused_result
-    private func placeOrder(direction:OrderDirection, _ symbol:String, _ price: Int, _ qty: Int, _ timeout:NSTimeInterval? = nil) -> Observable<OutstandingOrder> {
+    private func placeOrder(direction:OrderDirection, _ symbol:String, _ price: Int, _ qty: Int, _ timeout:NSTimeInterval? = nil) -> Observable<Order> {
         let pendingId:Int = lock(self) {
             _lastPendingOrderId += 1
             let p = _lastPendingOrderId
-            _pendingOrders[_lastPendingOrderId] = OutstandingOrder(id: p, symbol: symbol, price: price, qty: qty, direction: direction)
+            _pendingOrders[_lastPendingOrderId] = Order(id: p, symbol: symbol, price: price, qty: qty, direction: direction)
             return p
         }
         
         return _venue.placeOrderForStockAsync(symbol, price: price, qty: qty, direction: direction).map{ response in
-            let order = OutstandingOrder(
+            let order = Order(
                 id:response.id,
                 symbol: response.symbol,
                 price: response.price,
@@ -254,7 +261,7 @@ class TradingEngine {
                     guard let this = self else { return }
                     lock(this) {
                         if this._currentOrders[order.id] != nil {
-                            print("order timed out!")
+//                            print("order timed out!")
                             this.cancelOrder(order).subscribe()
                         }
                     }
@@ -272,14 +279,14 @@ class TradingEngine {
 
 /** Helpers and things which don't directly need to be in the engine go here */
 extension TradingEngine {
-    func cancelOrders(orders:[OutstandingOrder]) -> [OutstandingOrder] {
+    func cancelOrders(orders:[Order]) -> [Order] {
         for order in orders {
             cancelOrder(order).subscribe()
         }
         return orders
     }
     
-    func cancelOrdersForStock(symbol:String, predicate:(OutstandingOrder) -> Bool) -> [OutstandingOrder] {
+    func cancelOrdersForStock(symbol:String, predicate:(Order) -> Bool) -> [Order] {
         let ordersToCancel = _currentOrders.values.filter{ $0.symbol == symbol && predicate($0) } // we can't cancel pending orders
         return cancelOrders(Array(ordersToCancel)) // filter returns an array not a lazy sequence so this is ok
     }
--- StockFighter/level3-sell_side.swift
+++ StockFighter/level3-sell_side.swift
@@ -32,36 +32,37 @@ func sell_side(apiClient:StockFighterApiClient, _ gm:StockFighterGmClient) {
     let venue = apiClient.venue(account:tradingAccount, name:venueIdentifier)
     try! venue.heartbeat()
     
-    
     let engine = TradingEngine(apiClient: apiClient, account: tradingAccount, venue: venueIdentifier)
 
     engine.trackOrdersForStock(stockSymbol) { order in
         let isOutstanding = engine.outstandingOrdersForStock(stockSymbol).filter{ $0.id == order.id }.count > 0
         
         if order.open { return }
-        print("completed a \(order.direction): outstanding=\(isOutstanding) position=\(engine.positionForStock(stockSymbol)), profit=\(engine.netProfit)")
+        print("completed a \(order.direction): outstanding=\(isOutstanding) position=\(engine.positionForStock(stockSymbol))")
     }
     
-    let margin = 5 // under/overbid the market by x cents
-    let blockSize = 250 // it always sells in blockSize even if it doesn't have that many shares
-    
-    let outstandingBuys = { engine.outstandingOrdersForStock(stockSymbol).filter{ $0.direction == .Buy }.count }
-    let outstandingSells = { engine.outstandingOrdersForStock(stockSymbol).filter{ $0.direction == .Sell }.count }
+    let margin = 50 // under/overbid the market by x cents
+    let blockSize = 50 // it always sells in blockSize even if it doesn't have that many shares
     
     let arrayMin = { (items:[Int]) in items.reduce(Int.max) { min($0, $1) } }
     let arrayMax = { (items:[Int]) in items.reduce(0) { max($0, $1) } }
     
-    // this algorithm doesn't seem to work very well although my NAV does seem to creep up slowly over time
+    // this algorithm reliably makes money gradually and should win level 3 in about 10 minutes if you just leave it sitting there
+    // it tries to buy at a bit-higher than the min lowest buy price
+    // it tries to sell at a bit-lower than the min highest sell price
     //
-    // The code ends up selling for more than it's buying for a lot of the time. I'm not sure if that's just the market
-    // moving up and down, or if I need to do more tracking
+    // It has some safeguards:
     //
-    // I won the level by dumb luck. I cancelled when I had some stock, and the price
-    // went up on it's own
+    // - If the expected profit from this would be less than 50c, don't bother
+    //
+    // - Buy and sell in small blocks (50 or less) and set a short timeout (6s).
+    //   This means our orders don't sit around for too long to get exposed to the market moving
+    //
+    // - Adjust order quantity to buy/sell smaller amounts to compensate if we are too short/long in the stock
     
     engine.trackQuotesForStock(stockSymbol) { quote in
         
-        // just profile the market
+        // profile the market
         guard let bid = engine.mapReduceLastQuotes(7, map: { $0.bidBestPrice }, reduce: arrayMin),
             ask = engine.mapReduceLastQuotes(7, map: { $0.askBestPrice }, reduce: arrayMax) else
         {
@@ -71,31 +72,46 @@ func sell_side(apiClient:StockFighterApiClient, _ gm:StockFighterGmClient) {
         let expectedProfit = ask - bid
         if expectedProfit < 50 { return } // no point
 
-        // buy below market, sell above. It doesn't make sense, but we'll see if it works
-        let buyPrice = bid - margin
-        engine.cancelOrdersForStock(stockSymbol){ oo in oo.direction == .Buy && oo.price < buyPrice }
-        let sellPrice = ask + margin
-        engine.cancelOrdersForStock(stockSymbol){ oo in oo.direction == .Sell && oo.price > sellPrice }
+        // go inside the spread
+        let buyPrice = bid + margin
+        engine.cancelOrdersForStock(stockSymbol){ oo in oo.direction == .Buy && oo.price > buyPrice }
         
+        let sellPrice = ask - margin
+        engine.cancelOrdersForStock(stockSymbol){ oo in oo.direction == .Sell && oo.price < sellPrice }
+        
+        let outstandingBuys = engine.outstandingOrdersForStock(stockSymbol).filter{ $0.direction == .Buy }.count
+        let outstandingSells = engine.outstandingOrdersForStock(stockSymbol).filter{ $0.direction == .Sell }.count
         let position = engine.positionForStock(stockSymbol)
         
-        // buying stocks
-        if outstandingBuys() == 0 && position < 700 { // don't go too long
-            print("placing bid at \(buyPrice)")
-            engine.buyStock(stockSymbol, price: buyPrice, qty: blockSize, timeout: 30).subscribe()
+        let buffer = 500 // don't go long or short by more than this
+        let percentOutOfRange = Float(position) / Float(buffer)
+        
+        // buying a block and sell a block
+        if outstandingBuys == 0 && position < buffer { // only buy if not already buying. Slew the order size so as not to go too long
+            let qty = percentOutOfRange > 0 ?
+                Int(max(5, (Float(blockSize) * (1-percentOutOfRange)))) : // don't buy negative shares
+                blockSize
+            
+            print("placing bid for \(qty) at \(buyPrice)")
+            engine.buyStock(stockSymbol, price: buyPrice, qty: qty, timeout: 6).subscribe()
         }
         
-        // selling stocks
-        if outstandingSells() == 0 && position > 0 {
-            let sellCount = min(blockSize, position)  // don't sell things I don't have
-            print("placing ask for \(sellCount) at \(sellPrice)")
-            engine.sellStock(stockSymbol, price: sellPrice, qty: blockSize, timeout: 30).subscribe()
+        if outstandingSells == 0 && position > -buffer { // only sell if not already selling. Slew the order size so as not to go too short
+            let qty = percentOutOfRange < 0 ?
+                Int(max(5, (Float(blockSize) * (1+percentOutOfRange)))) : // don't sell negative shares
+                blockSize
+            
+            print("placing ask for \(qty) at \(sellPrice)")
+            engine.sellStock(stockSymbol, price: sellPrice, qty: qty, timeout: 6).subscribe()
         }
     }
     
     // keep the program running so async websocket doesn't terminate
-    print("press enter to stop")
-    let _  = readLine()
+    print("press q to stop")
+    while true {
+        let line = readLine()
+        if line == "q" { break }
+    }
     
     engine.close()
     
